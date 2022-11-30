b9c0589549c00afa68be9d961046aa5a07ea4a50
Mertcan Karabaş <64467624+MertcanKarabas@users.noreply.github.com>
2022-11-30 22:02:49 +0300

    Updates

--- execx.c
+++ execx.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     int i, f;
