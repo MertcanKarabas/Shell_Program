ddbf6a73acc95a1fa8827a071747777f766e0596
Mertcan Karabaş <64467624+MertcanKarabas@users.noreply.github.com>
2022-11-30 21:08:24 +0300

    Updates

--- deneme2.c
+++ deneme2.c
@@ -20,24 +20,39 @@ void inputaGoreFonk(char *string, char *input1[], char **input2) {
     }
 }
 
+void cat(char *input1[]) {
+    int i = 1;
+    printf("cat: ");
+    while(input1[i+1] != NULL) {
+        printf("%s ", input1[i]);
+        i++;
+    }
+    printf("%s\n", input1[i]);
+}
+
 int main(int argc, char *argv[], char** envp) {
 
     while(1)  {
     
-        int counter = 0;
+       
         char input[50];
         printf("MyShell >> ");
         fgets(input, 50, stdin);
-
-        char *split = strtok(input, " ");
-        char *inputs[10];
+        char *split = NULL;
+        split = strtok(input, " ");
+        char *inputs[100];
+        int counter = 0;
         while(split != NULL) {
             inputs[counter] = split;
             split = strtok(NULL, " ");
             counter++;
         }
-        
-        if ((strcmp(inputs[0], "exit\n") == 0)){
+        int i = 0;
+        while (inputs[i] != NULL) {
+            printf("%c", *inputs[i]);
+            i++;
+        }
+        if ((strcmp(inputs[0], "exit\n") == 0) ){
             return 0;
 
         } else if((strcmp(inputs[0], "ls\n") == 0)) {
@@ -48,8 +63,8 @@ int main(int argc, char *argv[], char** envp) {
 
             inputaGoreFonk("/bin/bash", inputs, envp);
 
-        } else if(strcmp(input, "cat\n") == 0) {
-            
+        } else if(strcmp(inputs[0], "cat") == 0) {
+            cat(inputs);
 
         } else if((strcmp(inputs[0], "clear\n") == 0)) {
             printf("\e[1;1H\e[2J");
@@ -57,6 +72,7 @@ int main(int argc, char *argv[], char** envp) {
         } else {
             printf("Yanlış argüman girildi\n");
         }
+        
         
     }
     return 0;
